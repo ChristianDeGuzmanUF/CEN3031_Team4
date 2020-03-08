ec77ee5201e31331be96d9b2f086cffae3d8bd3c
kdewey13 <kdewey@ufl.edu>
2020-03-08 16:58:14 -0400

    in progress

--- client/src/views/Register/Register.js
+++ client/src/views/Register/Register.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
+import sigil from './sigil.png';
 import './Register.css';
 
 function Register() {
+    const dashboard = () =>{
+        //needs fleshed out to determine student vs admin
+        window.location.href = "/Dashboard";
+    };
     return (
-        <div className="App">
-            <header className="App-header">                
-                <p>
-                    Register Page.
-                </p>
-                <a className="App-link" href="/RegisterCompleted">Register Completed</a> 
+        <div className="Register">
+            <header className="Register-header">
+                <p></p>
             </header>
+            <div className="Register-container">
+                <div className="Icon">
+                    <img src={sigil} alt="Logo" />;
+                </div>
+                <div className="Caption">
+
+                </div>
+                <div className="Register-Form">
+
+                </div>
+            </div>
+            <footer className="Register-header">
+                <p></p>
+            </footer>
         </div>
     );
 }
