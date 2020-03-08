b5d893cf88328c03862af5ae0297df5278e2e8bb
kdewey13 <kdewey@ufl.edu>
2020-03-08 19:24:21 -0400

    in progress

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
+import login from '../reg-pic.jpg'
 import './Login.css';
 
 function Login() {
+    const dashboard = () =>{
+        //needs fleshed out to determine student vs admin
+        window.location.href = "/Dashboard";
+    };
+    const register = () =>{
+        window.location.href = "/Register";
+    };
+    const recover = () =>{
+        window.location.href = "/RecoverPassword";
+    };
     return (
-        <div className="App">
-            <header className="App-header">                
-                <p>
-                    Login Page.
-                </p>
-                <a className="App-link" href="/Register">Register</a>
-                <a className="App-link" href="/RecoverPassword">Recover Password</a>                
-            </header>
+        <div className="Login">
+            <div className="Login-container">
+                <div className="Icon">
+                    <img src={login} alt="Logo" />
+                    <h1>Career Finder</h1>
+                    <h4>Account Login</h4>
+                </div>
+                <form className="Login-form">
+                    <div className="Login-form-col-1">
+                        <input type="text" placeholder="UserName"/>
+                        <input type="text" placeholder="Password"/>
+                    </div>
+                </form>
+                <div>
+                    <button onClick={dashboard}>Submit</button>
+                </div>
+                <div>
+                    <a href=onClick={dashboard}>Submit</a>
+                </div>
+                <div className="credits">
+                    Photo by Christin Hume on Unsplash
+                </div>
+            </div>
         </div>
     );
 }
