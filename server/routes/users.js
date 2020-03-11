def305807c36cf95793fcfa61f84d8f8f01d8d98
Travis Groesbeck <tsgbeck@gmail.com>
2020-03-10 23:13:31 -0700

    update to inviteCode registration

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,48 +5,48 @@ const config = require('../config/config');
 const validateUserRegInput = require('../controllers/registerUser');
 const validateUserLoginInput = require('../controllers/login');
 const User = require('../models/UserSchema');
-const validator = require('validator');
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
 
     // call function to validate registration input, and store returned errors
     // isValid is a boolean to indicate whether errors are present
-    const {errors, isValid} = validateUserRegInput(req.body);
+    const {errors, isValid, isAdminUser} = await validateUserRegInput(req.body);
 
     // if errors during registration, return 400 and json object of errors written
     if (!isValid) {
         return res.status(400).json(errors);
     };
 
-
     User.findOne({userName: req.body.userName}).then(user => {
-            if (user) {
-                return res.status(400).json({userName: "Username already exists"});
-            }
-            else {
-                const newUser = new User({
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    userName: req.body.userName,
-                    email: req.body.email,
-                    password: req.body.password1
-                });
+        if (user) {
+            return res.status(400).json({userName: "Username already exists"});
+        }
+        else {
+            // console.log("Is this an admin user? " + isAdminUser);
+            const newUser = new User({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                userName: req.body.userName,
+                email: req.body.email,
+                password: req.body.password1,
+                isAdmin: isAdminUser
+            });
 
-                // Hash password before saving in database
-                bcrypt.genSalt(10, (err, salt) => {
-                    bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if (err) throw err;
-                        newUser.password = hash;
-                        newUser
-                          .save()
-                          .then(user => res.json(user))
-                          .catch(err => console.log(err));
-                    });
+            // Hash password before saving in database
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                      .save()
+                      .then(user => res.json(user))
+                      .catch(err => console.log(err));
                 });
-            };
-        });
+            });
+        };
+    });
 });
 
 router.post("/login", (req, res) => {
