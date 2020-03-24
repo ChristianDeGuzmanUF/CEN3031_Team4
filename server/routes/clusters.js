9fccfcd245a914af1659de74a6304609fd31f17d
Travis Groesbeck <tsgbeck@gmail.com>
2020-03-24 16:47:17 -0700

    add searchByID for cluster records

--- server/routes/clusters.js
+++ server/routes/clusters.js
@@ -1,16 +1,37 @@
 const express = require('express');
-//const clusters = require('../controllers/clustersController.js');
 const Clusters = require('../models/ClusterSchema');
 
 const clustersRouter = express.Router();
 
 //clustersRouter.get("/clusters", clusters.list);
 
-clustersRouter.get("/", async (req, res) => {
-    console.log("made it here");
+clustersRouter.get('/', async (req, res) => {
     Clusters.find({})
-        .then(clusters => res.json(clusters))
+        .then(clusters => res.send(clusters))
         .catch(err => console.log(err));
 });
+
+clustersRouter.get('/:clusterID', async (req, res) => {
+    Clusters.findById(req.params.clusterID)
+        .then(cluster => {
+            if(!cluster) {
+                return res.status(404).send({
+                    message: "id not found with id " + req.params.clusterID
+                });
+            }
+        res.send(cluster);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(200).send({
+                    error: "ID not found with id " + req.params.clusterID
+                });
+            }
+            return res.status(500).send({
+                error : "Error retrieving cluster with id " + req.params.clusterID
+            });
+        });
+});
+
+
 
 module.exports = clustersRouter;
