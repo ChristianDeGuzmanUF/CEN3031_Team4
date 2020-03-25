a5ac1832d59a0e49eede5b1de24a029f640563ae
Travis Groesbeck <tsgbeck@gmail.com>
2020-03-24 17:07:10 -0700

    adding update option to cluster description

--- server/routes/clusters.js
+++ server/routes/clusters.js
@@ -32,6 +32,30 @@ clustersRouter.get('/:clusterID', async (req, res) => {
         });
 });
 
+clustersRouter.put('/:clusterID', async (req, res) => {
+
+    Clusters.findByIdAndUpdate(req.params.clusterID, {
+        description: req.body.description
+    }, {new: true})
+        .then(cluster => {
+            if(!cluster) {
+                return res.status(200).send({
+                    error: "ID not found with id " + req.params.clusterID
+                });
+            }
+            res.send(cluster);
+        }).catch(err => {
+            if(err.kind === 'ObjectId') {
+                return res.status(200).send({
+                    error: "ID not found with id " + req.params.clusterID
+                });
+            }
+            return res.status(500).send({
+                error: "Error updating ID with id " + req.params.clusterID
+            });
+        });
+});
+
 
 
 module.exports = clustersRouter;
