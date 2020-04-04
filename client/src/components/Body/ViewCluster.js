18399fd56b3649f1cb7c3b3231e8ed14be269f1d
kdewey13 <kdewey@ufl.edu>
2020-04-03 23:21:12 -0400

    boxes editable without erase

--- client/src/components/Body/ViewCluster.js
+++ client/src/components/Body/ViewCluster.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import clusterService from '../../actions/clusterService';
 
@@ -13,17 +14,27 @@ class ViewCluster extends Component {
             errors: {}
         };
     }
+
     onSubmit = e => {
         e.preventDefault();
 
         if (this.state.clusterName === "") {
             this.state.clusterName = this.props.selectedClusterData.clusterName;
         }
+        else {
+            this.state.clusterName = document.getElementById('clusterName').innerText;
+        }
         if (this.state.shortName === "") {
             this.state.shortName = this.props.selectedClusterData.shortName;
         }
+        else {
+            this.state.shortName = document.getElementById('shortName').innerText;
+        }
         if (this.state.description === "") {
             this.state.description = this.props.selectedClusterData.description;
+        }
+        else {
+            this.state.description = document.getElementById('description').innerText;
         }
 
         const clusterData = {
@@ -32,10 +43,9 @@ class ViewCluster extends Component {
             description: this.state.description
         };
 
-        clusterService.updateOne(this.props.selectedCluster, clusterData); 
+        clusterService.updateOne(this.props.selectedCluster, clusterData);
         this.props.getClusters();
     };
-
     render() {
         const { errors } = this.state;
         let theChosenOne = null;
@@ -44,57 +54,73 @@ class ViewCluster extends Component {
             this.props.selectedCluster !== null && this.props.selectedCluster !== ""
             && this.props.selectedClusterData !== null && this.props.selectedClusterData !== {}) {
             theChosenOne = this.props.selectedClusterData;
-                return (
-                    <div>
-                         <div className="crud-form-container">
-                             <div className="crud-title-small">Now viewing: {theChosenOne.shortName}</div>
-                             <div className="update-instructions">To make edits to this cluster, type changes into the form below and click the 'update' button at the bottom of the form.</div>
-                             <form className="general-form-area" noValidate onSubmit={this.onSubmit}>
-                                <div className="crud-single-column-col-1">
-                                    <div className="crud-form-title">
-                                        Cluster Name:
-
-                                    </div>
-                                    <textarea placeholder={theChosenOne.clusterName}
-                                              value={this.props.input}
-                                              onChange={(e) => {
-                                                  this.setState({clusterName: e.target.value})
-                                              }}
-                                    />
-                                    <span className="text-danger">
+            return (
+                <div>
+                    <div className="crud-form-container">
+                        <div className="crud-title-small">Now viewing: {theChosenOne.shortName}</div>
+                        <div className="update-instructions">To make edits to this cluster, type changes into the form below and click the 'update' button at the bottom of the form.</div>
+                        <form className="general-form-area" noValidate onSubmit={this.onSubmit}>
+                            <div className="crud-single-column-col-1">
+                                <div className="crud-form-title">
+                                    Cluster Name:
+                                </div>
+                                <div className="textareaElement"
+                                     contentEditable="true"
+                                     id = 'clusterName'
+                                     value={this.props.input}
+                                >{theChosenOne.clusterName}</div>
+                                <span className="text-danger">
                                         {errors.clusterName}
                                         </span>
-                                    <div className="crud-form-title">
-                                        Short name for the cluster (appears as the title):
-                                    </div>
-                                    <textarea placeholder={theChosenOne.shortName}
-                                              value={this.props.input}
-                                              onChange={(e) => {
-                                                  this.setState({shortName: e.target.value})
-                                              }}
-                                    />
-                                    <span className="text-danger">
+                                <div className="crud-form-title">
+                                    Short name for the cluster (appears as the title):
+                                </div>
+                                <div className="textareaElement"
+                                     contentEditable="true"
+                                     id = 'shortName'
+                                     value={this.props.input}
+                                     >{theChosenOne.shortName}</div>
+                                <span className="text-danger">
                                         {errors.shortName}
                                         </span>
-                                    <div className="crud-form-title">
-                                        Cluster Description:
-                                    </div>
-                                    <textarea placeholder={theChosenOne.description}
-                                              value={this.props.input}
-                                              onChange={(e) => {
-                                                  this.setState({description: e.target.value})
-                                              }}
-                                    />
-                                    <span className="text-danger">
+                                <div className="crud-form-title">
+                                    Cluster Description:
+                                </div>
+                                <div className="textareaElement"
+                                     contentEditable="true"
+                                     id = 'description'
+                                     value={this.props.input}
+                                     >{theChosenOne.description}</div>
+                                <span className="text-danger">
                                         {errors.description}
                                         </span>
-                                    <button className="wide-button" type="submit">Update</button>
+                                <div className="crud-form-title">
+                                    Skills for {theChosenOne.shortName}:
                                 </div>
-                            </form>
-                        </div>
-                        <p>{JSON.stringify(theChosenOne)}</p>
+                                <div className="textareaElement"
+                                     contentEditable="true"
+                                     value={this.props.input}
+                                     >{JSON.stringify(theChosenOne.skills)}</div>
+                                <span className="text-danger">
+                                        {errors.skills}
+                                        </span>
+                                <div className="crud-form-title">
+                                    Occupations in {theChosenOne.shortName}:
+                                </div>
+                                <div className="textareaElement"
+                                     contentEditable="true"
+                                     value={this.props.input}
+                                     >{JSON.stringify(theChosenOne.occupations)}</div>
+                                <span className="text-danger">
+                                        {errors.occupations}
+                                        </span>
+                                <br></br>
+                                <button className="wide-button" type="submit">Update</button>
+                            </div>
+                        </form>
                     </div>
-                );
+                </div>
+            );
         }
         else {
             return (
@@ -109,3 +135,81 @@ class ViewCluster extends Component {
     };
 }
 export default ViewCluster;
+{/*
+    render() {
+        const { errors } = this.state;
+        let theChosenOne = null;
+
+        if (this.props.clusters && this.props.clusters.length > 0 &&
+            this.props.selectedCluster !== null && this.props.selectedCluster !== ""
+            && this.props.selectedClusterData !== null && this.props.selectedClusterData !== {}) {
+            theChosenOne = this.props.selectedClusterData;
+            return (
+                <div>
+                    <div className="crud-form-container">
+                        <div className="crud-title-small">Now viewing: {theChosenOne.shortName}</div>
+                        <div className="update-instructions">To make edits to this cluster, type changes into the form below and click the 'update' button at the bottom of the form.</div>
+                        <form className="general-form-area" noValidate onSubmit={this.onSubmit}>
+                            <div className="crud-single-column-col-1">
+                                <div className="crud-form-title">
+                                    Cluster Name:
+
+                                </div>
+                                <textarea placeholder={theChosenOne.clusterName}
+                                          value={this.props.input}
+                                          onChange={(e) => {
+                                              this.setState({clusterName: e.target.value})
+                                          }}
+                                />
+                                <span className="text-danger">
+                                        {errors.clusterName}
+                                        </span>
+                                <div className="crud-form-title">
+                                    Short name for the cluster (appears as the title):
+                                </div>
+                                <textarea placeholder={theChosenOne.shortName}
+                                          value={this.props.input}
+                                          onChange={(e) => {
+                                              this.setState({shortName: e.target.value})
+                                          }}
+                                />
+                                <span className="text-danger">
+                                        {errors.shortName}
+                                        </span>
+                                <div className="crud-form-title">
+                                    Cluster Description:
+                                </div>
+                                <textarea placeholder={theChosenOne.description}
+                                          value={this.props.input}
+                                          onChange={(e) => {
+                                              this.setState({description: e.target.value})
+                                          }}
+                                />
+                                <span className="text-danger">
+                                        {errors.description}
+                                        </span>
+                                <button className="wide-button" type="submit">Update</button>
+                            </div>
+                        </form>
+                    </div>
+                    <p>{JSON.stringify(theChosenOne)}</p>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div className="crud-view-default">
+                    <p>
+                        <i>Click an item to view all of its information and to make changes.</i>
+                    </p>
+                </div>
+            );
+        }
+        return <div className="row">{theChosenOne}</div>;
+    };
+}
+export default ViewCluster;*/}
+
+
+
+
